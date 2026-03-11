0348101b83c9810e8037bc427fa194b35ada330e
jcyang1306 <jcyang@hkclr.hk>
2026-03-11 15:56:07 +0800

    - Fix typo

--- rm_moveit2_config/dual_arm_rm_65_config/launch/spawn_controllers.launch.py
+++ rm_moveit2_config/dual_arm_rm_65_config/launch/spawn_controllers.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_spawn_controllers_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("dual_rm65", package_name="dual_arm_   run").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("dual_rm65", package_name="dual_arm_rm_65_config").to_moveit_configs()
     return generate_spawn_controllers_launch(moveit_config)
